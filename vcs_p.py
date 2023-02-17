10ee572ee198309e6e625c5a3948ed22f1c94e48
Aleksander Kosarskii <kocagg@mail.ru>
2023-02-17 14:56:26 +0300

    feat : restructure work funcs

--- vcs_p.py
+++ vcs_p.py
@@ -8,67 +8,16 @@
 import os as OS
 import subprocess as SP
 import json as JSON
-import argparse as ARGP
-import shutil as Shutil
 import psutil as PU
 import time as Time
 import copy as Copy
 import config_args as ca
 import parse_args as pa
-
-def parse_args():
-    C_DESCRIPTION = "\nThe Tool to work with multi-repositories project. Based on VCS\
-                     \nAdditional information is in CONFIG FILE: " + ca.GC_VCS_P_CFG + "\
-                     \n\n!!! Please use only config file to work with branches !!!\
-                     \n!!! All commands are applying only to active profile !!!"
-
-    C_DESC_INIT = "primary initialization of repository"
-    C_DESC_CHECKOUT_B = "<branch_name> - create new branch for all repos"
-    C_DESC_CHECKOUT = "<branch_name> - checkout branch for all repos"
-    C_DESC_CFG_BRANCH = "<branch_name> - save branch to the config"
-    C_DESC_FETCH = "fetch all branches for all repos"
-    C_DESC_PULL = "pull all repos"
-    C_DESC_PUSH = "push all repos"
-    C_DESC_STATUS = "print status for all repos"
-    C_DESC_SHOW_REPOS = "show all actual repos"
-    C_DESC_CLEAR = "clear all meta data and repos"
-    C_DESC_EDIT = "edit config files and profiles"
-    C_DESC_SYNC = "sync profile - checkout all repos to the its branch"
-    C_DESC_PROFILE = "<branch_name> - create profile with name"
-    C_DESC_ADD = "use git add . command to all repos"
-    C_DESC_OREP = "<repo_name> - open repo in new terminal for manual fixes"
-    C_DESC_OALL = "open all repos in new terminal for manual fixes"
-    C_DESC_COMMIT = "open editor for commit message, then add git commit -m in all repos (tested editors: atom, nano)"
-    C_DESC_DOCKER = "open docker environment according to readme.md (press 'exit' to exit in docker).\
-                    Please use the link to set docker without root \
-                    https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user"
-
-    parser = ARGP.ArgumentParser(C_DESCRIPTION)
-    parser.add_argument(pa.GC_ARG_INIT, action="store_true", help=C_DESC_INIT)
-    parser.add_argument(pa.GC_ARG_CHECKOUT_B, action="store", type=str, default=None, help=C_DESC_CHECKOUT_B)
-    parser.add_argument(pa.GC_ARG_CHECKOUT, action="store", type=str, default=None, help=C_DESC_CHECKOUT)
-    parser.add_argument(pa.GC_ARG_CFG_BRANCH, action="store", type=str, default=None, help=C_DESC_CFG_BRANCH)
-    parser.add_argument(pa.GC_ARG_FETCH, action="store_true", help=C_DESC_FETCH)
-    parser.add_argument(pa.GC_ARG_PULL, action="store_true", help=C_DESC_PULL)
-    parser.add_argument(pa.GC_ARG_PUSH, action="store_true", help=C_DESC_PUSH)
-    parser.add_argument(pa.GC_ARG_STATUS, action="store_true", help=C_DESC_STATUS)
-    parser.add_argument(pa.GC_ARG_SHOW_REPOS, action="store_true", help=C_DESC_SHOW_REPOS)
-    parser.add_argument(pa.GC_ARG_CLEAR, action="store_true", help=C_DESC_CLEAR)
-    parser.add_argument(pa.GC_ARG_EDIT, action="store_true", help=C_DESC_EDIT)
-    parser.add_argument(pa.GC_ARG_SYNC, action="store_true", help=C_DESC_SYNC)
-    parser.add_argument(pa.GC_ARG_PROFILE, action="store", type=str, default=None, help=C_DESC_PROFILE)
-    parser.add_argument(pa.GC_ARG_ADD, action="store_true", help=C_DESC_ADD)
-    parser.add_argument(pa.GC_ARG_OREP, action="store", type=str, default=None, help=C_DESC_OREP)
-    parser.add_argument(pa.GC_ARG_OALL, action="store_true", help=C_DESC_OALL)
-    parser.add_argument(pa.GC_ARG_COMMIT, action="store_true", help=C_DESC_COMMIT)
-    parser.add_argument(pa.GC_ARG_DOCKER, action="store_true", help=C_DESC_DOCKER)
-
-    args = parser.parse_args()
-    return args
+import work_functions as wa
 
 
 def __get_profile():
-    return gp_profile
+    return ca.gp_profile
 
 
 def get_check_meta_info():
@@ -140,7 +89,7 @@ def get_check_repos_names():
 
 def __save_cfg_file():
     with open(ca.GC_VCS_P_CFG, 'w+') as f:
-        JSON.dump(G_CFG, f, indent=4)
+        JSON.dump(ca.G_CFG, f, indent=4)
 
 
 def __switch_branch_cfg(branch):
@@ -162,215 +111,38 @@ def init_repos_json():
         return False
 
     global G_CFG
-    if len(G_CFG["repos"]) != 0:
+    if len(ca.G_CFG["repos"]) != 0:
         print("skipping auto initialization of " + ca.GC_VCS_P_CFG)
         return False
 
     try:
         with open(ca.GC_VCS_P_CFG, 'r') as f:
-            G_CFG = JSON.load(f)
+            ca.G_CFG = JSON.load(f)
     except ValueError as ve:
         print("Error during JSON parsing: " + ca.GC_VCS_P_CFG + "\
             \n Message: " + str(ve))
         return False
 
-    if G_CFG == None:
+    if ca.G_CFG == None:
         print("Error: cannot load config " + G_CFG)
         return False
 
-    global gp_profile
-    gp_profile = G_CFG["profiles"][G_CFG["profile"]]
+    ca.gp_profile = G_CFG["profiles"][G_CFG["profile"]]
 
-    print("\nactive profile: " + gp_profile["branch_name"] + "\n" + str(gp_profile))
+    print("\nactive profile: " + ca.gp_profile["branch_name"] + "\n" + str(ca.gp_profile))
     return True
 
 
 def __add_profile(profile_name):
-    repos = G_CFG["repos"]
-    profiles = G_CFG["profiles"]
+    repos = ca.G_CFG["repos"]
+    profiles = ca.G_CFG["profiles"]
 
     profile = {}
     profile["branch_name"] = ca.GC_DEFAULT_BRANCH_NAME
     profile["repos"] = repos
 
     profiles[profile_name] = profile
-    G_CFG["profiles"] = profiles
-
-
-def work_init():
-    if not OS.path.exists(ca.GC_SRC_PATH):
-        OS.mkdir(ca.GC_SRC_PATH)
-
-    cmd = "vcs import " + ca.GC_SRC_PATH + " < " + ca.GC_REPOS_NAME
-    OS.system(cmd)
-
-    repos = []
-    entries = OS.scandir(ca.GC_SRC_PATH)
-    for entry in entries:
-        ename = entry.name
-        if not ename.startswith('.') and not entry.is_file():
-            repos.append(ename)
-            print("adding: " + ename + " ...")
-
-    G_CFG["repos"] = repos
-    __add_profile(ca.GC_DEFAULT_PROFILE_NAME)
-    __save_cfg_file()
-
-
-def work_show_repos():
-    isValid, repo_paths = get_check_repos_paths()
-    if not isValid:
-        print("Error: cannot load repos paths!" /
-              "Please check profiles/<active_profile>/repos in " + str(ca.GC_VCS_P_CFG))
-        return
-
-    isValid, repo_names = get_check_repos_names()
-    if not isValid:
-        print("Error: cannot load repos names!" /
-              "Please check profiles/<active_profile>/repos in " + str(ca.GC_VCS_P_CFG))
-        return
-
-    print("repo paths:")
-    for r in repo_paths:
-        print(r)
-
-    print("\nrepo names:")
-    for r in repo_names:
-        print(r)
-
-
-def work_status():
-    isValid, repos_paths = get_check_repos_paths()
-    if not isValid:
-        return
-
-    repos_str = __build_repos_str(repos_paths)
-    cmd = "vcs custom --git --repos " + repos_str + " --args status"
-    OS.system(cmd)
-    return
-
-
-def work_add_profile(profile):
-    __add_profile(profile)
-    print("New profile has been added: " + profile + "\
-        \nCheck it in editor")
-    __save_cfg_file()
-
-
-def work_checkoutb(branch):
-    isValid, repos_paths = get_check_repos_paths()
-    if not isValid:
-        return
-
-    __switch_branch_cfg(branch)
-    repos_str = __build_repos_str(repos_paths)
-    cmd = "vcs custom --git --repos " + repos_str + " --args checkout -b " + branch
-    OS.system(cmd)
-
-
-def work_checkout(branch):
-    isValid, repos_paths = get_check_repos_paths()
-    if not isValid:
-        return
-
-    __switch_branch_cfg(branch)
-    repos_str = __build_repos_str(repos_paths)
-
-    cmd = "vcs custom --git --repos " + repos_str + " --args checkout " + branch
-    OS.system(cmd)
-
-
-def work_fetch():
-    isValid, repos_paths = get_check_repos_paths()
-    if not isValid:
-        return
-
-    repos_str = __build_repos_str(repos_paths)
-    cmd = "vcs custom --git --repos " + repos_str + " --args fetch"
-    OS.system(cmd)
-
-
-def work_pull():
-    isValid, branch_name, _, repos_paths = get_check_meta_info()
-    if not isValid:
-        return
-
-    repos_str = __build_repos_str(repos_paths)
-    cmd = "vcs custom --git --repos " + repos_str + " --args pull origin " + branch_name
-    OS.system(cmd)
-
-
-def work_push():
-    isValid, branch_name, _, repos_paths = get_check_meta_info()
-    if not isValid:
-        return
-
-    repos_str = __build_repos_str(repos_paths)
-    cmd = "vcs custom --git --repos " + repos_str + " --args push -u origin " + branch_name
-    OS.system(cmd)
-
-
-def work_clear():
-    entries = OS.scandir(ca.GC_SRC_PATH)
-    for entry in entries:
-        ename = entry.name
-        if not ename.startswith('.') and not entry.is_file():
-            dir_path = OS.path.join(ca.GC_SRC_PATH, ename)
-            Shutil.rmtree(dir_path)
-            print("deleted: " + ename + " ...")
-
-    vcs_cfg_path = OS.path.join(ca.GC_FOLDER_PATH, ca.GC_VCS_P_CFG)
-    if not OS.path.exists(vcs_cfg_path):
-        print("cannot find config: " + vcs_cfg_path)
-    else:
-        OS.remove(vcs_cfg_path)
-        print("config is deleted: " + vcs_cfg_path)
-    return
-
-
-def work_sync():
-    work_checkout(gp_profile["branch_name"])
-    return
-
-
-def work_orep(repo):
-    isValid, repos_names = get_check_repos_names()
-    if not isValid:
-        return
-
-    if repo not in repos_names:
-        print("Bad repo name (string supposed): " + str(repo) + \
-              "\nCheck repo name in active profile in " + ca.GC_VCS_P_CFG + \
-              "\nor use " + str(pa.GC_ARG_SHOW_REPOS) + " to show" + \
-              "\nActual repos: " + str(repos_names)
-              )
-        return
-
-    cmd = "gnome-terminal --working-directory=$(pwd)/src/" + repo
-    OS.system(cmd)
-    return
-
-
-def work_oallrepos():
-    isValid, repos_paths = get_check_repos_paths()
-    if not isValid:
-        return
-
-    for repo in repos_paths:
-        cmd = "gnome-terminal --working-directory=" + repo
-        OS.system(cmd)
-
-    return
-
-
-def work_add():
-    isValid, repos_names = get_check_repos_paths()
-    if not isValid:
-        return
-
-    repos_str = __build_repos_str(repos_names)
-    cmd = "vcs custom --git --repos " + repos_str + " --args add ."
-    OS.system(cmd)
+    ca.G_CFG["profiles"] = profiles
 
 
 def __commit_default_editor(commit_file_path):
@@ -391,100 +163,61 @@ def __commit_alternative_editor(commit_file_path):
     return
 
 
-def work_commit():
-    isValid, branch_name, _, repos_paths = get_check_meta_info()
-    if not isValid:
-        return
-
-    with open(ca.GC_COMMIT_FILE_NAME, 'w+') as f:
-        f.close()
-
-    commit_file_path = OS.path.join(ca.GC_FOLDER_PATH, ca.GC_COMMIT_FILE_NAME)
-    if G_CFG["editor_commit"] == ca.GC_EDITOR_COMMIT_DEFAULT:
-        __commit_default_editor(commit_file_path)
-    else:
-        __commit_alternative_editor(commit_file_path)
-
-    commit_message = ""
-    with open(ca.GC_COMMIT_FILE_NAME, 'r') as f:
-        commit_message = f.read()
-
-    if not __ch_str(commit_message):
-        print("Please specify commit message in opened editor\n" \
-              "Error: Cannot find a commit message\n" \
-              "Check: \" " + G_CFG["editor_commit"] + "\" field in main JSON: " + ca.GC_VCS_P_CFG
-              )
-        return
-
-    commit_message += "\n On branch " + branch_name + \
-                      " \n pm-Development: " + G_CFG["autor"]
-
-    repos_str = __build_repos_str(repos_paths)
-    cmd = "vcs custom --git --repos " + repos_str + " --args commit -m \"" + commit_message + "\""
-    OS.system(cmd)
-
-    OS.remove(commit_file_path)
-    return
-
-
-def work_docker_open():
-    OS.system(ca.GC_DOCKER_OPEN_CMD)
-
 
 def parse_work():
-    args = parse_args()
+    args = pa.parse_args()
 
     if args.clear:
-        work_clear()
+        wa.work_clear()
 
     if args.init:
-        work_init()
+        wa.work_init()
 
     if not init_repos_json():
         return
 
     print("\nSync started... \n\n")
-    work_sync()
+    wa.work_sync()
     print("\nSync completed! Working on direct command... \n\n")
 
     if pa.__ch_str(args.orep):
-        work_orep(args.orep)
+        wa.work_orep(args.orep)
 
     if args.oallrepos:
-        work_oallrepos()
+        wa.work_oallrepos()
 
     if pa.__ch_str(args.profile):
-        work_add_profile(args.profile)
+        wa.work_add_profile(args.profile)
 
     if pa.__ch_str(args.checkoutb):
-        work_checkoutb(args.checkoutb)
+        wa.work_checkoutb(args.checkoutb)
 
     if pa.__ch_str(args.checkout):
-        work_checkout(args.checkout)
+        wa.work_checkout(args.checkout)
 
     if pa.__ch_str(args.cfgb):
         __switch_branch_cfg(args.cfgb)
 
     if args.show_repos:
-        work_show_repos()
+        wa.work_show_repos()
 
     if args.fetch:
-        work_fetch()
+        wa.work_fetch()
 
     if args.status:
-        work_status()
+        wa.work_status()
 
     if args.pull:
-        work_pull()
+        wa.work_pull()
 
     if args.push:
-        work_push()
+        wa.work_push()
 
     if args.sync:
-        work_sync()
+        wa.work_sync()
 
     if args.add:
-        work_add()
+        wa.work_add()
 
     if args.commit:
         work_commit()
